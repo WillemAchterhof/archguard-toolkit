07719ddf4295633d8fc2befefe0d9e6b9523d6e9
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-18 11:25:04 +0200

    Create apply-updates.sh

---
+++ lib/apply-updates.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+# Apply Updates
+# ------------------------------------------------------------------------------
+# /lib/apply-updates.sh
+
+source "$ROOT_LIB/check-repository.sh"
+source "$ROOT_LIB/toolkit-update.sh"
+
+apply_updates()
+{
+    check_repository \
+        "$ROOT_BACKUP/.archguard-configs" \
+        "force-remote"
+
+    toolkit_update
+}
